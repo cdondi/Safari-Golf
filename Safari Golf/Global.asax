a066d457edfc9881982e48b486f9537c8123ec14
Clive D. <clivedondi@hotmail.com>
2017-01-25 19:48:02 -0600

    Initial commit.

---
+++ Safari Golf/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Safari_Golf.MvcApplication" Language="C#" %>
---
+++ Safari Golf/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Safari_Golf.Startup))]
+namespace Safari_Golf
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
