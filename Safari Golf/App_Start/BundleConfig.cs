beea0ac880ca2d1208f91735e2dca226ab5a1de6
Clive D. <clivedondi@hotmail.com>
2017-01-26 19:11:44 -0600

    Changed theme from lumen to sandstone.

--- Safari Golf/App_Start/BundleConfig.cs
+++ Safari Golf/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Safari_Golf
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap-sandstone.css",
                       "~/Content/site.css"));
         }
     }
