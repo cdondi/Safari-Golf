1f5f97aee0652ac5ad88334eceada506a9da7ef5
Clive D. <clivedondi@hotmail.com>
2017-01-27 13:08:59 -0600

    Completed about us page.

--- Safari Golf/Controllers/HomeController.cs
+++ Safari Golf/Controllers/HomeController.cs
@@ -15,14 +15,12 @@ namespace Safari_Golf.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Information";
 
             return View();
         }
